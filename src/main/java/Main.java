8f1f695cbb44204ff4bdd14e529134ae080b5e46
pr23106082-Yesith <hansana.etec1216@example.com>
2024-07-07 09:28:07 +0530

    another Funtions

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/AddCusFormController.fxml"))));
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/welcomeForm.fxml"))));
         //stage.initStyle(StageStyle.UNDECORATED);
         stage.show();
 
